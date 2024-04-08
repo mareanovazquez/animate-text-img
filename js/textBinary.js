b1f9fec45b497d0b78cde1ff5c7af650a2c83f44
Mariano Vazquez <mareanovazquez@protonmail.com>
2024-04-08 13:43:04 -0300

    ajustes en text binary

--- js/textBinary.js
+++ js/textBinary.js
@@ -4,21 +4,35 @@ document.addEventListener("DOMContentLoaded", function () {
     // Obtenemos el texto definitivo
     let textoDefinitivo = textBinaryElement.textContent;
 
+    /* SI SE METE ESTE CÓDIGO ADENTRO DEL setTimeout la palabra final
+    no se muestra hasta que no termina el map. 
+    Eso sí, se tiene que elminar el que está repetido afuera
+    
+     // Generamos una nueva combinación de ceros y unos aleatorios
+    let binaryAleatorio = Array(textoDefinitivo.length).fill('').map(function () {
+        return Math.random() < 0.5 ? '0' : '1'; // Generamos un 0 o un 1 aleatoriamente
+    });
+
+    // Mostramos los ceros y unos aleatorios antes del texto definitivo
+    textBinaryElement.textContent = binaryAleatorio.join('');
+    */
+
+    // Generamos una nueva combinación de ceros y unos aleatorios
+    let binaryAleatorio = Array(textoDefinitivo.length).fill('').map(function () {
+        return Math.random() < 0.5 ? '0' : '1'; // Generamos un 0 o un 1 aleatoriamente
+    });
+
+    // Mostramos los ceros y unos aleatorios antes del texto definitivo
+    textBinaryElement.textContent = binaryAleatorio.join('');
+
     // Función para mostrar el texto definitivo letra por letra
     function mostrarTextBinary(letraIndex) {
         setTimeout(function () {
-            // Generamos una nueva combinación de ceros y unos aleatorios
-            let binaryAleatorio = Array(textoDefinitivo.length).fill('').map(function () {
-                return Math.random() < 0.5 ? '0' : '1'; // Generamos un 0 o un 1 aleatoriamente
-            });
-         
-            // Mostramos los ceros y unos aleatorios antes del texto definitivo
-            textBinaryElement.textContent = binaryAleatorio.join('');
 
-                // Actualizamos el texto en el elemento con el carácter correspondiente del texto definitivo
+            // Actualizamos el texto en el elemento con el carácter correspondiente del texto definitivo
             let nuevoTexto = textBinaryElement.textContent.substring(0, letraIndex) + textoDefinitivo.charAt(letraIndex) + textBinaryElement.textContent.substring(letraIndex + 1);
             textBinaryElement.textContent = nuevoTexto;
-            
+
             // Llamamos recursivamente a la función para mostrar la siguiente letra
             if (letraIndex < textoDefinitivo.length - 1) {
                 mostrarTextBinary(letraIndex + 1);
